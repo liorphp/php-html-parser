5aceabc9f981fcdd5ebc36a351d0b1b17ffccaaa
Lior.php <39471599+liorphp@users.noreply.github.com>
2024-05-16 01:10:35 +0300

    Update TextNode.php

--- src/PHPHtmlParser/Dom/Node/TextNode.php
+++ src/PHPHtmlParser/Dom/Node/TextNode.php
@@ -57,7 +57,7 @@ class TextNode extends LeafNode
         }
 
         // restore line breaks
-        $text = \parse_str($text);
+        $text = (string)$text;
         $text = \str_replace('&#10;', "\n", $text);
 
         $this->text = $text;
